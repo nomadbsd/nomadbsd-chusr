296b08a36545294939ff7fa71bc019e66e211801
Marcel <mk@freeshell.de>
2022-08-01 18:05:10 +0200

    Initial commit.

---
+++ locale/nomadbsd-chusr_fr.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../nomadbsd-chusr" line="83"/>
+        <source>General settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="84"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="87"/>
+        <source>&amp;Delete account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="428"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="428"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="122"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="123"/>
+        <source>Repeat password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="125"/>
+        <source>Change password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="139"/>
+        <source>Passwords do not match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="141"/>
+        <source>Password must not be empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="417"/>
+        <source>pw returned with an error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="154"/>
+        <source>Password successfully changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="163"/>
+        <source>User:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="168"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="171"/>
+        <source>Login group:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="176"/>
+        <source>Secondary groups:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="179"/>
+        <source>Login class:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="182"/>
+        <source>Shell:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="208"/>
+        <source>Discard changes?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="208"/>
+        <source>Changes have not been saved. Discard?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="208"/>
+        <source>&amp;Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="428"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="379"/>
+        <source>Unknown group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="379"/>
+        <source>Unknown group &quot;{0}&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="397"/>
+        <source>Saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="401"/>
+        <source>Sure to delete account?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="401"/>
+        <source>Are you sure you want to delete the account for user {0}?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="401"/>
+        <source>Delete account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="401"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="420"/>
+        <source>Account deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="428"/>
+        <source>Really quit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="428"/>
+        <source>Quit without saving?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="440"/>
+        <source>Modified</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../nomadbsd-chusr" line="470"/>
+        <source>Error: You must be root to run this program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nomadbsd-chusr" line="474"/>
+        <source>Permission denied</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplications</name>
+    <message>
+        <location filename="../nomadbsd-chusr" line="472"/>
+        <source>You must be root to run this program</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
